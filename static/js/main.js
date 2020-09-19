3c0e5231ac892c94c11a748d6bda4cad67e2b9d2
Manato1 <39127254+Manato1@users.noreply.github.com>
2020-09-20 00:37:33 +0200

    Upload Static Files

---
+++ static/js/main.js
@@ -0,0 +1,82 @@
+function startLoad() {
+    // Dark = 0, Light = 1
+    var colorMode = 0;
+    var cookie = getCookie("colorMode");
+    if (cookie) {
+        colorMode = cookie;
+    }
+
+    if (colorMode > 0) {
+        document.body.classList.add('light_mode');
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        document.getElementById("loader").classList.add("done");
+        addFadeOut();
+    })
+
+    window.addEventListener('pageshow', function (event) {
+        if (!event.persisted) {
+            return;
+        }
+        var fader = document.getElementById('fader');
+        fader.classList.remove('fade-in');
+    });
+}
+
+function addFadeOut() {
+    if (!window.AnimationEvent) { return }
+
+    var anchors = document.getElementsByTagName('a');
+
+    for (var idx = 0; idx < anchors.length; idx += 1) {
+
+        if (anchors[idx].hostname !== window.location.hostname) {
+            continue;
+        }
+
+        anchors[idx].addEventListener('click', function (event) {
+
+            var fader = document.getElementById('loader'),
+                anchor = event.currentTarget;
+
+            event.preventDefault();
+            fader.classList.remove('done');
+
+            setTimeout(function () {
+                window.location = anchor.href;
+            }, 1010)
+        });
+    }
+
+}
+
+function toggleColorMode() {
+    document.body.classList.toggle('light_mode');
+
+    if (document.body.classList.contains('light_mode')) {
+        document.cookie = "colorMode=1"
+    } else {
+        document.cookie = "colorMode=0"
+    }
+}
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+            end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
