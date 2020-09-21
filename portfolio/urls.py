ff099b84f327b3e19212078ee5e4e56fc297cc17
Manato1 <39127254+Manato1@users.noreply.github.com>
2020-09-21 15:27:02 +0200

    Styling für Mobilgeräte aktualisiert

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,7 +3,7 @@ from portfolio import views
 
 app_name = 'me'
 urlpatterns = [
-    path('', views.portfolio, name='about_me'),
+    path('', views.portfolio, name='home'),
     path('about_me', views.portfolio, name='about_me'),
     path('curriculum_vitae', views.cv, name='curriculum_vitae'),
     path('contact', views.contact, name='contact')
